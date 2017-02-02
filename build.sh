01f6897d5f0241ab71c8e700fe34e583881c070d
Bo Maryniuk <bo@suse.de>
2017-02-02 15:48:22 +0100

    Add src build script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function get_version() {
+    echo $(./git-format-pkg-patch -v | sed -e 's/.*: //g')
+}
+
+
+function get_archive_name() {
+    echo "git-packaging-tools-$(get_version).tar.gz"
+}
+
+
+function cleanup() {
+    if [ -d "$1" ]; then
+	rm -rf $1
+    fi
+}
+
+dirname="git-packaging-tools-$(get_version)"
+cleanup $dirname
+mkdir $dirname
+cp git-format-pkg-patch $dirname
+tar cvf - $dirname | gzip -9 > $(get_archive_name)
+rm -rf $dirname
+echo "Done"
