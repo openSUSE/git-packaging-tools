f94f6b8672b37fc1d2dabf8c70f020b04ae28a87
Bo Maryniuk <bo@suse.de>
2017-02-02 15:48:15 +0100

    Add python setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='git-packaging-tools',
+    version='0.2',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Bo Maryniuk',
+    author_email='bo@suse.de',
+    description='Git to OBS tools'
+)
